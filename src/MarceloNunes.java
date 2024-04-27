1bc5e725038eead7815396d942fa22f7288ae068
Marcelo Santos <marcello.santos@gmail.com>
2024-04-26 21:10:27 -0300

    Criação do projeto e da classe inicial

---
+++ src/MarceloNunes.java
@@ -0,0 +1,9 @@
+public class MarceloNunes {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá aluno");
+        System.out.println("Crie uma classe com seu nome e sobrenome");
+        System.out.println("Salve e me envie o resultado!");
+    }
+
+}
